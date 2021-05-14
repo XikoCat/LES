d1f86db5e94e384d3b5718cd13954ad22e37af55
XikoCat <guigasreis99@gmail.com>
2021-05-14 09:48:12 +0100

    Code Cleanup

--- Evento/models.py
+++ Evento/models.py
@@ -1,22 +1,21 @@
 from django.db import models
 
-'id', 'tipo_de_eventoid', 'nome', 'estado', 'descrição', 'data', 'hora', 'duração', 'local', 'valor', 'evento_pagoid' 
 
 class Evento(models.Model):
-    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    tipo_de_eventoid = models.ForeignKey('TipoDeEvento', models.SET_NULL, default=None, null=True, db_column='Tipo de eventoID')  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    proponenteutilizadorid = models.ForeignKey('main.Proponente', models.SET_NULL, default=None, null=True, db_column='ProponenteUtilizadorID')  # Field name made lowercase.
-    formulárioinscriçãoid = models.ForeignKey('Formulario.Formulário', models.SET_NULL, default=None, null=True, db_column='FormulárioInscriçãoID')  # Field name made lowercase.
-    formuláriofeedbackid = models.ForeignKey('Formulario.Formulário', models.SET_NULL, default=None, null=True, related_name = 'feedback', db_column='FormulárioFeedbackID')  # Field name made lowercase.
-    nome = models.CharField(db_column='Nome', max_length=255, blank=True, null=True)  # Field name made lowercase.
-    estado = models.CharField(db_column='Estado', max_length=255, blank=True, null=True)  # Field name made lowercase.
-    descrição = models.CharField(db_column='Descrição', max_length=255, blank=True, null=True)  # Field name made lowercase.
-    data = models.DateField(db_column='Data', blank=True, null=True)  # Field name made lowercase.
-    hora = models.TimeField(db_column='Hora', blank=True, null=True)  # Field name made lowercase.
-    duração = models.IntegerField(db_column='Duração', blank=True, null=True)  # Field name made lowercase.
-    local = models.IntegerField(db_column='Local', blank=True, null=True)  # Field name made lowercase.
-    valor = models.FloatField(db_column='Valor')  # Field name made lowercase.
-    evento_pagoid = models.IntegerField(db_column='Evento pagoID')  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    id = models.AutoField(db_column='ID', primary_key=True) 
+    tipo_de_eventoid = models.ForeignKey('TipoDeEvento', models.SET_NULL, default=None, null=True, db_column='Tipo de eventoID') 
+    proponenteutilizadorid = models.ForeignKey('main.Proponente', models.SET_NULL, default=None, null=True, db_column='ProponenteUtilizadorID') 
+    formulárioinscriçãoid = models.ForeignKey('Formulario.Formulário', models.SET_NULL, default=None, null=True, db_column='FormulárioInscriçãoID') 
+    formuláriofeedbackid = models.ForeignKey('Formulario.Formulário', models.SET_NULL, default=None, null=True, related_name = 'feedback', db_column='FormulárioFeedbackID') 
+    nome = models.CharField(db_column='Nome', max_length=255, blank=True, null=True) 
+    estado = models.CharField(db_column='Estado', max_length=255, blank=True, null=True) 
+    descrição = models.CharField(db_column='Descrição', max_length=255, blank=True, null=True) 
+    data = models.DateField(db_column='Data', blank=True, null=True) 
+    hora = models.TimeField(db_column='Hora', blank=True, null=True) 
+    duração = models.IntegerField(db_column='Duração', blank=True, null=True) 
+    local = models.IntegerField(db_column='Local', blank=True, null=True) 
+    valor = models.FloatField(db_column='Valor') 
+    evento_pagoid = models.IntegerField(db_column='Evento pagoID') 
 
     class Meta:
         managed = True
@@ -24,12 +23,12 @@ class Evento(models.Model):
 
 
 class CertificadoDeParticipação(models.Model):
-    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    eventoid = models.ForeignKey('Evento', models.SET_NULL, default=None, null=True, db_column='EventoID')  # Field name made lowercase.
-    nome_evento = models.CharField(db_column='Nome evento', max_length=255, blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    data_emissão = models.DateField(db_column='Data emissão', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    nome_participante = models.CharField(db_column='Nome participante', max_length=255, blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    publico = models.IntegerField(db_column='Publico')  # Field name made lowercase.
+    id = models.AutoField(db_column='ID', primary_key=True) 
+    eventoid = models.ForeignKey('Evento', models.SET_NULL, default=None, null=True, db_column='EventoID') 
+    nome_evento = models.CharField(db_column='Nome evento', max_length=255, blank=True, null=True) 
+    data_emissão = models.DateField(db_column='Data emissão', blank=True, null=True) 
+    nome_participante = models.CharField(db_column='Nome participante', max_length=255, blank=True, null=True) 
+    publico = models.IntegerField(db_column='Publico') 
 
     class Meta:
         managed = True
@@ -37,30 +36,30 @@ class CertificadoDeParticipação(models.Model):
 
         
 class PedidoDeRecurso(models.Model):
-    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    eventoid = models.ForeignKey('Evento', models.SET_NULL, default=None, null=True, db_column='EventoID')  # Field name made lowercase.
-    tipo_de_recursoid = models.ForeignKey('Recurso.TipoDeRecurso', models.SET_NULL, default=None, null=True, db_column='Tipo de recursoID')  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    quantidade = models.IntegerField(db_column='Quantidade')  # Field name made lowercase.
-    dia_inicial = models.DateField(db_column='Dia inicial', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    hora_inicial = models.TimeField(db_column='Hora inicial', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    dia_final = models.DateField(db_column='Dia final', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    hora_final = models.TimeField(db_column='Hora final', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    capacidade = models.IntegerField(db_column='Capacidade', blank=True, null=True)  # Field name made lowercase.
-    tipo_de_equipamentoid = models.ForeignKey('Recurso.TipoDeEquipamento', models.SET_NULL, default=None, null=True, db_column='Tipo de equipamentoID')  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    tipo_de_serviçoid = models.ForeignKey('Recurso.TipoDeServiço', models.SET_NULL, default=None, null=True, db_column='Tipo de serviçoID')  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    id = models.AutoField(db_column='ID', primary_key=True) 
+    eventoid = models.ForeignKey('Evento', models.SET_NULL, default=None, null=True, db_column='EventoID') 
+    tipo_de_recursoid = models.ForeignKey('Recurso.TipoDeRecurso', models.SET_NULL, default=None, null=True, db_column='Tipo de recursoID') 
+    quantidade = models.IntegerField(db_column='Quantidade') 
+    dia_inicial = models.DateField(db_column='Dia inicial', blank=True, null=True) 
+    hora_inicial = models.TimeField(db_column='Hora inicial', blank=True, null=True) 
+    dia_final = models.DateField(db_column='Dia final', blank=True, null=True) 
+    hora_final = models.TimeField(db_column='Hora final', blank=True, null=True) 
+    capacidade = models.IntegerField(db_column='Capacidade', blank=True, null=True) 
+    tipo_de_equipamentoid = models.ForeignKey('Recurso.TipoDeEquipamento', models.SET_NULL, default=None, null=True, db_column='Tipo de equipamentoID') 
+    tipo_de_serviçoid = models.ForeignKey('Recurso.TipoDeServiço', models.SET_NULL, default=None, null=True, db_column='Tipo de serviçoID') 
 
     class Meta:
         managed = True
         db_table = 'Pedido de recurso'
 
 class Requisição(models.Model):
-    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    eventoid = models.OneToOneField('Evento.Evento', models.SET_NULL, default=None, null=True, db_column='EventoID')  # Field name made lowercase.
-    recursoid = models.OneToOneField('Recurso.Recurso', models.SET_NULL, default=None, null=True, db_column='RecursoID')  # Field name made lowercase.
-    dia_inicial = models.DateField(db_column='Dia inicial', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    hora_inicial = models.TimeField(db_column='Hora inicial', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    dia_final = models.DateField(db_column='Dia final', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    hora_final = models.TimeField(db_column='Hora final', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    id = models.AutoField(db_column='ID', primary_key=True) 
+    eventoid = models.OneToOneField('Evento.Evento', models.SET_NULL, default=None, null=True, db_column='EventoID') 
+    recursoid = models.OneToOneField('Recurso.Recurso', models.SET_NULL, default=None, null=True, db_column='RecursoID') 
+    dia_inicial = models.DateField(db_column='Dia inicial', blank=True, null=True) 
+    hora_inicial = models.TimeField(db_column='Hora inicial', blank=True, null=True) 
+    dia_final = models.DateField(db_column='Dia final', blank=True, null=True) 
+    hora_final = models.TimeField(db_column='Hora final', blank=True, null=True) 
 
     class Meta:
         managed = True
@@ -69,8 +68,8 @@ class Requisição(models.Model):
 
 
 class TipoDeEvento(models.Model):
-    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    nome = models.CharField(db_column='Nome', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    id = models.AutoField(db_column='ID', primary_key=True) 
+    nome = models.CharField(db_column='Nome', max_length=255, blank=True, null=True) 
 
     class Meta:
         managed = True
